f72ff12f89e22d3c5fa5d43e1ea28b15eb47c7d0
Shaun9887 <101912535+Shaun9887@users.noreply.github.com>
2022-03-22 10:03:23 -0400

    Add files via upload

---
+++ jasmine-testing-exercises/calculator/calculator.js
@@ -0,0 +1,58 @@
+window.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById("calc-form");
+  if (form) {
+    setupInitialValues();
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      update();
+    });
+  }
+});
+
+function getCurrentUIValues() {
+  return {
+    amount: +(document.getElementById("loan-amount").value),
+    years: +(document.getElementById("loan-years").value),
+    rate: +(document.getElementById("loan-rate").value),
+  }
+}
+
+// Get the inputs from the DOM.
+// Put some default values in the inputs
+// Call a function to calculate the current monthly payment
+function setupInitialValues() {
+  const values = { amount: 10000, years: 10, rate: 4.5 };
+  const userAmount = document.getElementById('loan-amount');
+  userAmount.value = values.amount;
+  const userYears = document.getElementById('loan-years');
+  userYears.value = values.years;
+  const userRate = document.getElementById('loan-rate');
+  userRate.value = values.rate;
+  update();
+}
+
+// Get the current values from the UI
+// Update the monthly payment
+function update() {
+  const currentValues = getCurrentUIValues();
+  updateMonthly(calculateMonthlyPayment(currentValues));
+}
+
+// Given an object of values (a value has amount, years and rate ),
+// calculate the monthly payment.  The output should be a string
+// that always has 2 decimal places.
+function calculateMonthlyPayment(values) {
+  const monthlyRate = (values.rate / 100) / 12;
+  const n = Math.floor(values.years * 12);
+  return (
+    (monthlyRate * values.amount) /
+    (1 - Math.pow((1 + monthlyRate), -n))
+  ).toFixed(2);
+}
+
+// Given a string representing the monthly payment value,
+// update the UI to show the value.
+function updateMonthly(monthly) {
+  const monthlyUI = document.getElementById("monthly-payment");
+  monthlyUI.innerText = "$" + monthly;
+}
